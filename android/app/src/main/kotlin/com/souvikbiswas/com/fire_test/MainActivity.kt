78abaeb35a3ae295f2c5a232f6666594bf719aa8
Souvik Biswas <sbis1999@gmail.com>
2020-11-14 14:10:53 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/souvikbiswas/com/fire_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.souvikbiswas.com.fire_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fire Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fire Test'),
+      ),
+      body: Container(),
+    );
+  }
+}
