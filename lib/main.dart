63a74e5cf30b72df49bd1ff3606df0754d942625
Souvik Biswas <sbis1999@gmail.com>
2020-11-14 18:16:41 +0530

    Added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(MyApp());
 }
 
@@ -24,7 +30,50 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Fire Test'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+                child: const Text('Key'),
+                onPressed: () {
+                  FirebaseCrashlytics.instance.setCustomKey('foo', 'bar');
+                }),
+            FlatButton(
+                child: const Text('Log'),
+                onPressed: () {
+                  FirebaseCrashlytics.instance.log('Simulated crash');
+                }),
+            FlatButton(
+                child: const Text('Crash'),
+                onPressed: () {
+                  FirebaseCrashlytics.instance.crash();
+                }),
+            FlatButton(
+                child: const Text('Throw Error'),
+                onPressed: () {
+                  throw StateError('Uncaught error thrown by app.');
+                }),
+            FlatButton(
+                child: const Text('Async out of bounds'),
+                onPressed: () {
+                  Future<void>.delayed(const Duration(seconds: 2), () {
+                    final List<int> list = <int>[];
+                    print(list[100]);
+                  });
+                }),
+            FlatButton(
+              child: const Text('Record Error'),
+              onPressed: () {
+                try {
+                  throw 'error_example';
+                } catch (error, stackTrace) {
+                  FirebaseCrashlytics.instance.recordError(error, stackTrace);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
